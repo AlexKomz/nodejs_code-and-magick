c427d963a473fe18be7f8bbacc8e6cce81ff1309
KVC\komzoltcev <komzoltcev.a@gmail.com>
2021-05-21 12:33:49 +0300

    Переписал метод execute() через promisify(fs.writeFile) вместо возвращения нового промиса

--- src/generate.js
+++ src/generate.js
@@ -1,31 +1,41 @@
 const fs = require(`fs`);
 const colors = require(`colors`);
+const {promisify} = require(`util`);
 const {generateEntity} = require(`./generator/wizards-generator`);
+
+const writeFile = promisify(fs.writeFile);
 
 const COMMAND = `--generate`;
 const DEFAULT_PATH = `${process.cwd()}/wizards.json`;
 
 const data = generateEntity();
-const fileWriteOptions = {
-  encoding: `utf-8`,
-  mode: 0o644
-};
+// const fileWriteOptions = {
+//   encoding: `utf-8`,
+//   mode: 0o644
+// };
 
 module.exports = {
   isApplicable(command) {
     return command === COMMAND;
   },
   execute(path = DEFAULT_PATH) {
-    return new Promise((resolve, reject) => {
-      fs.writeFile(path, JSON.stringify(data), fileWriteOptions, (err) => {
-        if (err) {
-          err.message = colors.red(err.message);
-          return reject(err);
-        }
-
-        const message = `Data was generated into > ${path}`;
-        return resolve(colors.green(message));
-      });
-    });
+    return writeFile(path, JSON.stringify(data))
+      .catch((err) => {
+        return Promise.reject(colors.red(err.message));
+      })
+      .then(() => Promise.resolve(colors.green(`Data was generated into > ${path}`)));
   }
+  // execute(path = DEFAULT_PATH) {
+  //   return new Promise((resolve, reject) => {
+  //     fs.writeFile(path, JSON.stringify(data), fileWriteOptions, (err) => {
+  //       if (err) {
+  //         err.message = colors.red(err.message);
+  //         return reject(err);
+  //       }
+  //
+  //       const message = `Data was generated into > ${path}`;
+  //       return resolve(colors.green(message));
+  //     });
+  //   });
+  // }
 };
