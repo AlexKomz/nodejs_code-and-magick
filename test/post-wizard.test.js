c83ec93b38038be59391d933c5ac9e58f6a523ec
KVC\komzoltcev <komzoltcev.a@gmail.com>
2021-05-27 10:43:51 +0300

    Добавил тесты для GET и POST запросов

---
+++ test/post-wizard.test.js
@@ -0,0 +1,72 @@
+const request = require(`supertest`);
+const assert = require(`assert`);
+
+const app = require(`../src/server`).app;
+
+describe(`POST /api/wizards`, () => {
+  it(`send wizard as json`, async () => {
+    const sent = {
+      name: `SuperWizard`
+    };
+
+    const response = await request(app).
+      post(`/api/wizards`).
+      send(sent).
+      set(`Accept`, `application/json`).
+      set(`Content-Type`, `application/json`).
+      expect(200).
+      expect(`Content-Type`, /json/);
+
+    const wizard = response.body;
+    assert.deepStrictEqual(wizard, sent);
+  });
+
+  it(`send wizard without name`, async () => {
+    const response = await request(app).
+      post(`/api/wizards`).
+      send({}).
+      set(`Accept`, `application/json`).
+      set(`Content-Type`, `application/json`).
+      expect(400).
+      expect(`Content-Type`, /json/);
+
+
+    const errors = response.body;
+    assert.deepStrictEqual(errors, [
+      `Field name 'name' is required!`
+    ]);
+  });
+
+  it(`send wizard as multipart/form-data`, async () => {
+    const wizardName = `SuperWizard`;
+
+    const response = await request(app).
+      post(`/api/wizards`).
+      field(`name`, wizardName).
+      set(`Accept`, `application/json`).
+      set(`Content-Type`, `multipart/form-data`).
+      expect(200).
+      expect(`Content-Type`, /json/);
+
+
+    const wizard = response.body;
+    assert.deepStrictEqual(wizard, {name: wizardName});
+  });
+
+  it(`send wizard with avatar as multipart/form-data`, async () => {
+    const wizardName = `SuperWizard`;
+
+    const response = await request(app).
+      post(`/api/wizards`).
+      field(`name`, wizardName).
+      attach(`avatar`, `test/fixtures/keks.png`).
+      set(`Accept`, `application/json`).
+      set(`Content-Type`, `multipart/form-data`).
+      expect(200).
+      expect(`Content-Type`, /json/);
+
+
+    const wizard = response.body;
+    assert.deepStrictEqual(wizard, {name: wizardName, avatar: {name: `keks.png`}});
+  });
+});
